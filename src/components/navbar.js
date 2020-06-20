3c72927a0fef26f08f16ecddd5c1ceb9b51ba46b
Nathaniel Okenwa <nokenwa@chatterboxcoder.me>
2020-06-21 00:47:19 +0100

    initialised Gatsby Site

---
+++ src/components/navbar.js
@@ -0,0 +1,28 @@
+import { Link } from "gatsby";
+
+const NavBar = () => (
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/media">Media</Link>
+        </li>
+        <li>
+          <Link to="/events">Events</Link>
+        </li>
+        <li>
+          <Link to="/blog">Blog</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+);
