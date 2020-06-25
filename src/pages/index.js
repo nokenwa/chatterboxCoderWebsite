be489f47db5ebf00ca7261dc11b5e5039cabafec
Nathaniel Okenwa <nokenwa@chatterboxcoder.me>
2020-06-25 19:26:59 +0100

    Added more Social Links to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
+import sexyAsFuck from "../images/saf.png";
 
 import styled from "styled-components";
 
@@ -55,12 +56,19 @@ const Socials = styled.div`
 	width: 100%;
 `;
 
+const GraphicImage = styled.img`
+	position: absolute;
+	bottom: 2em;
+	right: 10em;
+`;
+
 const IndexPage = () => (
 	<Layout>
 		{/* <SEO title="Home" /> */}
 		<TitleDiv>
 			<TitleBack />
 			<Title>Chatterbox Coder</Title>
+			{/* LINE */}
 			<svg
 				xmlns="http://www.w3.org/2000/svg"
 				width="60%"
@@ -76,9 +84,62 @@ const IndexPage = () => (
 			</svg>
 			<Subtitle>Nathaniel Okenwa</Subtitle>
 			<Socials>
-				<SocialLink href="" title="twitch page"></SocialLink>
+				<SocialLink
+					href="https://www.twitch.tv/chatterboxcoder"
+					title="twitch page"
+				>
+					<svg width="1.1em" height="100%" viewBox="0 0 2400 2800">
+						<g>
+							<g id="Layer_1-2">
+								<path
+									class="st0"
+									d="M500,0L0,500v1800h600v500l500-500h400l900-900V0H500z M2200,1300l-400,400h-400l-350,350v-350H600V200h1600
+					   V1300z"
+									fill="#FFFFFF"
+								/>
+								<rect
+									x="1700"
+									y="550"
+									class="st0"
+									width="200"
+									height="600"
+									fill="#FFFFFF"
+								/>
+								<rect
+									x="1150"
+									y="550"
+									class="st0"
+									width="200"
+									height="600"
+									fill="#FFFFFF"
+								/>
+							</g>
+						</g>
+					</svg>
+				</SocialLink>
 				{/* Twitch */}
-				<SocialLink href="" title="Twitter page"></SocialLink> {/* Twitter */}
+				<SocialLink
+					href="https://twitter.com/chatterboxcoder"
+					title="Twitter page"
+				>
+					<svg height="100%" width="100%" viewBox="0 0 400 400">
+						<g id="Logo__x2014__FIXED">
+							<g>
+								<path
+									class="st1"
+									d="M153.6,301.6c94.3,0,145.9-78.2,145.9-145.9c0-2.2,0-4.4-0.1-6.6c10-7.2,18.7-16.3,25.6-26.6
+			c-9.2,4.1-19.1,6.8-29.5,8.1c10.6-6.3,18.7-16.4,22.6-28.4c-9.9,5.9-20.9,10.1-32.6,12.4c-9.4-10-22.7-16.2-37.4-16.2
+			c-28.3,0-51.3,23-51.3,51.3c0,4,0.5,7.9,1.3,11.7c-42.6-2.1-80.4-22.6-105.7-53.6c-4.4,7.6-6.9,16.4-6.9,25.8
+			c0,17.8,9.1,33.5,22.8,42.7c-8.4-0.3-16.3-2.6-23.2-6.4c0,0.2,0,0.4,0,0.7c0,24.8,17.7,45.6,41.1,50.3c-4.3,1.2-8.8,1.8-13.5,1.8
+			c-3.3,0-6.5-0.3-9.6-0.9c6.5,20.4,25.5,35.2,47.9,35.6c-17.6,13.8-39.7,22-63.7,22c-4.1,0-8.2-0.2-12.2-0.7
+			C97.7,293.1,124.7,301.6,153.6,301.6"
+									fill="#ffffff"
+								/>
+							</g>
+						</g>
+					</svg>
+				</SocialLink>
+				{/* Twitter */}
 				<SocialLink
 					href="https://www.instagram.com/chatterboxcoder/"
 					title="instagram page"
@@ -101,7 +162,21 @@ const IndexPage = () => (
 					</svg>
 				</SocialLink>
 				{/* Instagram */}
-				<SocialLink href="" title="github profile"></SocialLink> {/* Github */}
+				<SocialLink href="https://github.com/nokenwa" title="github profile">
+					<svg
+						class="octicon octicon-mark-github v-align-middle"
+						height="100%"
+						viewBox="0 0 16 16"
+						width="1.2em"
+					>
+						<path
+							fill-rule="evenodd"
+							d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"
+							fill="#FFFFFF"
+						></path>
+					</svg>
+				</SocialLink>
+				{/* Github */}
 				<SocialLink
 					href="https://www.linkedin.com/in/nathanielokenwa/"
 					title="linkedIn profile"
@@ -140,6 +215,8 @@ const IndexPage = () => (
 				{/* Linked In */}
 			</Socials>
 		</TitleDiv>
+
+		<GraphicImage src={sexyAsFuck} />
 	</Layout>
 );
 
