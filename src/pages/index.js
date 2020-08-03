9e340ccdb1138e2d0679e2b3a58b1abbf9f6aeeb
Nathaniel Okenwa <nokenwa@chatterboxcoder.me>
2020-08-03 11:48:16 +0100

    Added Redirects to Publish Folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
-import sexyAsFuck from "../images/saf.png";
+import graphicLogo from "../images/chatterbox.png";
 
 import styled from "styled-components";
 
@@ -216,7 +216,7 @@ const IndexPage = () => (
 			</Socials>
 		</TitleDiv>
 
-		<GraphicImage src={sexyAsFuck} />
+		<GraphicImage src={graphicLogo} />
 	</Layout>
 );
 
